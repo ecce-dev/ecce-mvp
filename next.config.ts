a9f3eef647248a6f9e15a7978b0f82128001d87f
kla <253251140+ecce-dev@users.noreply.github.com>
2026-01-08 15:30:46 +0100

    fix manifest loading for PWA

--- next.config.ts
+++ next.config.ts
@@ -40,7 +40,6 @@ const pwaConfig = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
 export default pwaConfig(nextConfig);
